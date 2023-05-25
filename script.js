28aeb2983fa696b569581976fa860d36e925666c
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-05-25 17:03:46 +0200

    Created password generator

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("JS OK")
+console.log('JS OK')
+
+// recupero l'elemento dal Dom
+
+const pwPlaceHolder = document.getElementById('pw-gen');
+console.log(pwPlaceHolder);
+
+// Chiedo il nome all'utente
+
+const userName = prompt('Qual è il tuo nome?' , 'Giacomo');
+console.log(userName);
+
+// Chiedo il cognome all'utente
+
+const userSurname = prompt('Qual è il tuo cognome?' , 'Arcangeli');
+console.log(userSurname);
+
+// Chiedo il colore preferito all'utente
+
+const userColor = prompt('Qual è il tuo colore preferito?' , 'Verde');
+console.log(userColor);
+
+// Genero la password
+
+const newPassword = userName + userSurname + userColor + 21;
+console.log(newPassword);
+
+// Inserisco il risultato nell'elemento recuperato dal DOM
+
+pwPlaceHolder.innerText = newPassword;
---
+++ steps.md
@@ -0,0 +1,15 @@
+# Traccia
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+<br>
+
+## Steps
+1. Recupero l'elemento dal DOM
+2. Chiedo il nome all'utente
+3. Chiedo il cognome all'utente
+4. Chiedo il colore preferito all'utente
+5. Genero la password
+6. Inserisco il risultato nell'elemento recuperato dal DOM
